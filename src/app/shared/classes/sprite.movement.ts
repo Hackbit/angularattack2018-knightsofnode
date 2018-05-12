e7333a3db5f5a44c3cb050c5366b21b4b55ba21c
Bradley Brown <tall.white.ninja@gmail.com>
2018-05-12 00:35:20 -0600

    created sprite movement class with barebones, non-obstacle aware logic

---
+++ src/app/shared/classes/sprite.movement.ts
@@ -0,0 +1,14 @@
+export class spriteMovement {
+    moveUp(yPosition): number {
+        return yPosition + 25;
+    };
+    moveDown(yPosition): number {
+        return yPosition - 25;
+    };
+    moveLeft(xPosition): number {
+        return xPosition - 25;
+    };
+    moveRight(xPosition): number {
+        return xPosition - 25;
+    };
+}
