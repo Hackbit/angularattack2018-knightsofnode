8e63c3bfb4820b469bef7bc49de50a376bf23a6d
Bradley Brown <tall.white.ninja@gmail.com>
2018-05-11 22:31:51 -0600

    PoC of moving object with keypress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import {RouterModule, Routes} from "@angular/router";
 import {SplashComponent} from "./splash/splash.component";
+import {BoardComponent} from "./board/board.component";
 import {UserService} from "./shared/services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
 import {WindowSizeService} from "./shared/services/window.size.service";
@@ -8,6 +9,7 @@ import {WindowSizeService} from "./shared/services/window.size.service";
 export const allAppComponents = [SplashComponent];
 
 export const routes: Routes = [
+	{path: "/board", component: BoardComponent},
 	{path: "", component: SplashComponent}
 ];
 
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,31 @@
+import { Component, HostListener, AfterViewInit } from "@angular/core";
+import * as createjs from 'createjs-module';
+
+@Component({
+    template: require("./board.component.html")
+})
+
+export class BoardComponent implements AfterViewInit {
+
+    ngAfterViewInit() {
+        var stage = new createjs.Stage("demoCanvas");
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+        circle.x = 10;
+        circle.y = 10;
+        stage.addChild(circle);
+
+        stage.update();
+
+
+
+        createjs.Ticker.setFPS(60);
+
+        document.addEventListener('keypress', function (event) {
+            createjs.Tween.get(circle, { loop: false })
+                .to({ x: circle.x + 10 }, 1000, createjs.Ease.getPowInOut(4))
+            stage.update();
+        });
+    }
+
+}
