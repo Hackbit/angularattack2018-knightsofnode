77e314bab3e4772e4ab3bcbb274c5dfda811df69
Bradley Brown <tall.white.ninja@gmail.com>
2018-05-11 22:38:14 -0600

    users are dead

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,24 +1,15 @@
 import {Component, OnInit} from "@angular/core";
-import {User} from "../shared/classes/user";
-import {UserService} from "../shared/services/user.service";
 import {WindowSizeService} from "../shared/services/window.size.service";
 
 @Component({
 	template: require("./splash.component.html")
 })
 
-export class SplashComponent implements OnInit{
-	users: User[] = [];
+export class SplashComponent {
 
-	constructor(protected windowSizeService: WindowSizeService, protected userService: UserService) {}
+	constructor(protected windowSizeService: WindowSizeService) {}
 
 	isMobile(): boolean {
 		return(this.windowSizeService.isMobile());
 	}
-
-	ngOnInit():void {
-		this.userService.getAllUsers()
-			.subscribe(users => this.users = users);
-	}
-
 }
