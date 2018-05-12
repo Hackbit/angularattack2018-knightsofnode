d9bd86adb471e803245c9a3d88b2524a23f53382
Dylan McDonald <dmcdonald21@cnm.edu>
2018-05-11 21:57:40 -0600

    put placeholders for emitting breakpoint changing events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
-import {Component} from "@angular/core";
+import {Component, HostListener, OnInit} from "@angular/core";
 
 @Component({
 	selector: "knights-of-node",
 	template: require("./app.component.html")
 })
 
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	deviceWidth: number = null;
+	SM_BREAKPOINT: number = 768;
+
+	isMobile(): boolean {
+		return(this.deviceWidth < this.SM_BREAKPOINT);
+	}
+
+	ngOnInit(): void {
+		this.deviceWidth = window.innerWidth;
+	}
+
+	@HostListener("window:resize", ["$event"])
+	onResize(event): void {
+		let newWidth = event.target.innerWidth;
+		if(this.deviceWidth !== null && (this.deviceWidth < this.SM_BREAKPOINT && newWidth >= this.SM_BREAKPOINT)) {
+			console.log("mobile -> desktop");
+		} else if (this.deviceWidth !== null && (this.deviceWidth >= this.SM_BREAKPOINT && newWidth < this.SM_BREAKPOINT)) {
+			console.log("desktop -> mobile");
+		}
+		this.deviceWidth = newWidth;
+	}
+}
