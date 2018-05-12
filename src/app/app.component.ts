befb90d266c12c5a09a9826e979e39e391961424
Dylan McDonald <dmcdonald21@cnm.edu>
2018-05-11 23:25:08 -0600

    added placeholder for wasd control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,22 @@ export class AppComponent {
 
 	constructor(protected windowSizeService: WindowSizeService) {}
 
+	@HostListener("document:keyup", ["$event"])
+	onKeyUpEvent(keyEvent: KeyboardEvent) {
+		let noMetaKeys: boolean = keyEvent.altKey === false && keyEvent.ctrlKey === false && keyEvent.metaKey == false && keyEvent.repeat === false;
+		let key: string = keyEvent.key.toLowerCase();
+
+		if(noMetaKeys === true && key === "w") {
+			// up
+		} else if(noMetaKeys === true && key === "a") {
+			// left
+		} else if(noMetaKeys === true && key === "s") {
+			// down
+		} else if(noMetaKeys === true && key === "d") {
+			// right
+		}
+	}
+
 	@HostListener("window:resize", ["$event"])
 	onResize(event): void {
 		this.windowSizeService.windowResize({height: event.target.innerHeight, width: event.target.innerWidth});
