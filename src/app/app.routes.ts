bd1f5f34761b2b8a71e85e17e61b7291861163bc
Dylan McDonald <dmcdonald21@cnm.edu>
2018-05-13 12:40:15 -0600

    added team component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import {RulesComponent} from "./rules/rules.component";
 import {WindowSizeService} from "./shared/services/window.size.service";
 import {PlayerControlService} from "./shared/services/player.control.service";
 import {HeartbeatService} from "./shared/services/heartbeat.service";
+import {TeamComponent} from "./team/team.component";
 
 
-export const allAppComponents = [BoardComponent, RulesComponent, SplashComponent];
+export const allAppComponents = [BoardComponent, RulesComponent, SplashComponent, TeamComponent];
 
 export const routes: Routes = [
 	{path: "board", component: BoardComponent},
 	{path: "rules", component: RulesComponent},
+	{path: "team", component: TeamComponent},
 	{path: "", component: SplashComponent}
 ];
 
---
+++ src/app/team/team.component.ts
@@ -0,0 +1,6 @@
+import {Component} from "@angular/core";
+
+@Component({
+	template: require("./team.component.html")
+})
+export class TeamComponent {}
