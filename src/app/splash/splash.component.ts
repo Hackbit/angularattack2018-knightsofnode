6e6876e67537c5ca3dc7903dec7fd3375bea0326
Dylan McDonald <dmcdonald21@cnm.edu>
2018-11-11 19:55:25 -0800

    converted icons on the splash

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,9 +1,12 @@
 import {Component} from "@angular/core";
+import {faAngular, faJs, faReact} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
 	template: require("./splash.component.html")
 })
 
 export class SplashComponent {
-
+	faAngular = faAngular;
+	faJs = faJs;
+	faReact = faReact;
 }
