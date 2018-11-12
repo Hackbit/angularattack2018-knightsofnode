f35e0f21815067eac87d7de04fa4dd3f5d2ae7a8
Dylan McDonald <dmcdonald21@cnm.edu>
2018-11-11 19:57:59 -0800

    converted icons on the team

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,6 +1,10 @@
 import {Component} from "@angular/core";
+import {faGithub, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
 	template: require("./team.component.html")
 })
-export class TeamComponent {}
+export class TeamComponent {
+	faGithub = faGithub;
+	faTwitter = faTwitter;
+}
