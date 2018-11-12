42b7cf20100f98801a0be829d3d1a1940ab5fc54
Dylan McDonald <dmcdonald21@cnm.edu>
2018-11-11 19:52:42 -0800

    converted icons on the rules

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,6 +1,12 @@
 import {Component} from "@angular/core";
+import {faArrowDown, faArrowLeft, faArrowRight, faArrowUp} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
 	template: require("./rules.component.html")
 })
-export class RulesComponent {}
+export class RulesComponent {
+	faArrowDown = faArrowDown;
+	faArrowLeft = faArrowLeft;
+	faArrowRight = faArrowRight;
+	faArrowUp = faArrowUp;
+}
